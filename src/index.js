73cdec5377eea7f13f2949e5944fe296adaebfd1
Dmytro Ilhanaiev <ilhanaiev.dmytro@email.cz>
2017-06-22 10:03:37 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import './styles.css';
+
+var showHome = function() {
+	var home = document.getElementById("list"),
+		about = document.getElementById("about"),
+		contacts = document.getElementById("contacts");
+	home.style.display = "block";
+	about.style.display = "none";
+	contacts.style.display = "none";
+}
+
+var showAbout = function() {
+	var home = document.getElementById("list"),
+		about = document.getElementById("about"),
+		contacts = document.getElementById("contacts");
+	home.style.display = "none";
+	about.style.display = "block";
+	contacts.style.display = "none";
+}
+
+var showContacts = function() {
+	var home = document.getElementById("list"),
+		about = document.getElementById("about"),
+		contacts = document.getElementById("contacts");
+	home.style.display = "none";
+	about.style.display = "none";
+	contacts.style.display = "block";
+}
---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const webpack = require('webpack'),
+	path = require('path'),
+	HtmlPlugin = require('html-webpack-plugin'),
+	ExtractTextPlugin = require('extract-text-webpack-plugin'),
+	merge = require('webpack-merge')
+
+const PATHS = {
+	src: path.join(__dirname, "src"),
+	dist: path.join(__dirname, "dist")
+}
+
+const common = {
+	entry: PATHS.src + "/index.js",
+	output: {
+		filename: '[name].js',
+		path: PATHS.dist
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: ExtractTextPlugin.extract({
+					use: [
+						//'style-loader',
+						'css-loader'
+					]
+				})
+			},
+			{
+				test: /\.(jpe?g|gif|png|svg)$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: 'images/[name].[ext]'
+						}
+					}
+				]
+			}
+		]
+	},
+	plugins: [
+		new HtmlPlugin({
+			filename: 'index.html',
+			template: PATHS.src + '/main.html'
+		}),
+		new ExtractTextPlugin('./style.css')
+	]
+}
+
+const dev = merge(
+	common
+)
+
+const prod = merge(
+	common
+)
+
+module.exports = function() {
+	return dev
+}
