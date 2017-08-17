b8e3cb27444598d72f4245bb0a983fba21750b9d
Dmytro Ilhanaiev <ilhanaiev.dmytro@email.cz>
2017-08-17 03:23:47 +0200

    verification

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ app.use(express.static(__dirname + '/dist'));
 app.get('/', function(req,res){
 	res.render('/index.html')
 });
+app.get('/google885327df46f587f4.html', function(req,res){
+	res.sendFile('/google885327df46f587f4.html')
+});
 
 app.listen(app.get('port'), function() {
 	console.log('Node app is running on port', app.get('port'));
